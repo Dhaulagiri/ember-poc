76e8844d513897b1a018b0a2d76bc01cd294d7ff
Brian Runnells <brian.runnells@gmail.com>
2015-03-20 15:02:30 -0500

    Add 5 second "streaming"

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
+var TIMEOUT = 5000;
+var timeoutFunc;
+
 export default Ember.Route.extend({
   queryParams: {
     symbol: {
@@ -16,7 +19,24 @@ export default Ember.Route.extend({
   },
   actions: {
     updateSymbol: function(symbol) {
+      // cancel the timeout
+      clearTimeout(timeoutFunc);
       this.transitionTo({queryParams: {symbol: symbol}});
     }
+  },
+  afterModel: function(model) {
+    this.refreshQuote(model);
+  },
+
+  refreshQuote: function(model) {
+
+    var symbol = model.quote.Symbol;
+    var url = `http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=${symbol}&callback=?`;
+
+    Ember.$.getJSON(url).then(function(res) {
+      Ember.set(model, 'quote', res)
+    });
+
+    timeoutFunc = setTimeout(this.refreshQuote.bind(this, model), TIMEOUT);
   }
 });
